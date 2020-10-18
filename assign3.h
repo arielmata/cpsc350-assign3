78ec5d9220e6864ae4fe5e9ef0a1aa03ef4dc7d0
Ariel Mata <ariel@Ariels-MacBook-Air.local>
2020-10-18 16:38:26 -0700

    Makefile and header file

---
+++ assign3.h
@@ -0,0 +1,23 @@
+// header file - assign3.h
+/*
+Full name: Ariel Gutierrez
+Student ID: 2318163
+Chapman email: arigutierrez@chapman.edu
+Course number and section: CPSC 350-01
+Assignment or exercise number: Assignment 2
+
+Assignment 2: Program that implements the Game of Life, a simulation that
+models the life cycle of bacteria.
+*/
+#include <iostream> //input and output
+#include <fstream> //read and open files
+#include <stdlib.h> //srand, rand, RAND_MAX
+#include <sstream> //stringstream
+#include <ctime> //current time function
+#include <unistd.h> //sleep function
+using namespace std;
+
+
+class GenStack{
+  //generic stack with template
+}
